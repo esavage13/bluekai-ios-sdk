c0bb46cae6a45bc20e4fb025096266a9c07ead67
Shun Chu <shun@bluekai.com>
2014-04-15 19:44:23 -0700

    attempt to fix compile errors

--- sample/Bluekai/TestViewController.m
+++ sample/Bluekai/TestViewController.m
@@ -1,93 +1,102 @@
-
 #import "TestViewController.h"
 #import "Bluekai.h"
 
+@interface TestViewController ()
+@end
 
 @implementation TestViewController
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
     if (self) {
         //self.title = NSLocalizedString(@"BlueKai", @"BlueKai");
         self.tabBarItem.title=@"BlueKai";
     }
     return self;
 }
-							
+
+-(void)loadView {
+
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 -(IBAction)sendKeyValuePair:(id)sender
 {
-    if(key_Txtfld.text.length==0)
+    if(keyTextfield.text.length==0)
     {
         alert=[[UIAlertView alloc]initWithTitle:@"Error message" message:@"Enter key" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
         alert.delegate=self;
         [alert show];
-        [alert release];
     }
     else
     {
-        if(value_Txtfld.text.length==0)
+        if(valueTextfield.text.length==0)
         {
            alert=[[UIAlertView alloc]initWithTitle:@"Error message" message:@"Enter value" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
             alert.delegate=self;
             [alert show];
-            [alert release];
         }
         else
         {
-            [obj_SDK put:key_Txtfld.text :value_Txtfld.text];
+            [blueKaiSDK put:keyTextfield.text withValue:valueTextfield.text];
         }
     }
 }
+
 -(void)appCameToForeGround
 {
     NSLog(@"Application opened");
-    [obj_SDK resume];
+    [blueKaiSDK resume];
 }
+
 -(void)onDataPosted:(BOOL)status
 {
     if(status)
     {
         alert=[[UIAlertView alloc]initWithTitle:nil message:@"\n\nData sent successfully" delegate:self cancelButtonTitle:nil otherButtonTitles:nil, nil];
         [alert show];
-        [alert release];
     }
     else {
         alert=[[UIAlertView alloc]initWithTitle:nil message:@"\n\nData could not be sent" delegate:self cancelButtonTitle:nil otherButtonTitles:nil, nil];
         [alert show];
-        [alert release];
     }
     [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(removeAlert:) userInfo:nil repeats:NO];
 }
+
 -(void)removeAlert:(id)sender
 {
     [alert dismissWithClickedButtonIndex:-1 animated:YES];
 }
+
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [textField resignFirstResponder];
     return YES;
 }
+
 -(IBAction)cancelBtn:(id)sender
 {
-    key_Txtfld.text=@"";
-    value_Txtfld.text=@"";
+    keyTextfield.text = @"";
+    valueTextfield.text = @"";
 }
+
 -(void)viewWillAppear:(BOOL)animated
 {
      [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appCameToForeGround) name:UIApplicationWillEnterForegroundNotification object:nil];
     self.tabBarController.delegate=self;
-   // [obj_SDK release];
-    //[config_dict release];
+   // [blueKaiSDK release];
+    //[configDict release];
     NSFileManager *fileManager = [NSFileManager defaultManager];
     NSError *error;
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
-    NSString *plistPath = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"Configurationfile.plist"];
+    NSString *plistPath = [paths[0] stringByAppendingPathComponent:@"Configurationfile.plist"];
     
     BOOL success = [fileManager fileExistsAtPath:plistPath];
     if(!success){
@@ -95,9 +104,9 @@
         NSString *defaultPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Configurationfile.plist"];
         [fileManager copyItemAtPath:defaultPath toPath:plistPath error:&error];
     }
-    config_dict=[[NSMutableDictionary alloc]initWithContentsOfFile:plistPath];
+    configDict = [[NSMutableDictionary alloc]initWithContentsOfFile:plistPath];
     
-//    if(![[config_dict objectForKey:@"devMode"] boolValue])
+//    if(![[configDict objectForKey:@"devMode"] boolValue])
 //    {
 //        NSArray *subviews=[self.view subviews];
 //        for (UIView *view in subviews) {
@@ -114,23 +123,21 @@
 //            }
 //        }
 //    }
-    NSLog(@"%@",[config_dict objectForKey:@"devMode"]);
-if([[config_dict objectForKey:@"devMode"]boolValue])
-{
-    obj_SDK=[[BlueKai alloc]initWithArgs:YES withSiteId:[config_dict objectForKey:@"siteId"] withAppVersion:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] withView:self];
-}
-else{
-     obj_SDK=[[BlueKai alloc]initWithArgs:NO withSiteId:[config_dict objectForKey:@"siteId"] withAppVersion:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] withView:self];
-}
-    obj_SDK.delegate=self;
-   // obj_SDK=[[Bluekai_SDK alloc]initWithBool:NO withSiteId:@"2" withAppVersion:@"1" withView:self];
+    NSLog(@"DevMode? ======= %@", configDict[@"devMode"]);
+    if([configDict[@"devMode"]boolValue])
+    {
+        blueKaiSDK=[[BlueKai alloc]initWithArgs:YES withSiteId:configDict[@"siteId"] withAppVersion:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] withView:self];
+    }
+    else{
+         blueKaiSDK=[[BlueKai alloc]initWithArgs:NO withSiteId:configDict[@"siteId"] withAppVersion:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"] withView:self];
+    }
+    blueKaiSDK.delegate=self;
+   // blueKaiSDK=[[Bluekai_SDK alloc]initWithBool:NO withSiteId:@"2" withAppVersion:@"1" withView:self];
 }
 
 - (void)viewDidUnload
 {
     [super viewDidUnload];
-    [obj_SDK release];
-    [config_dict release];
    
     // Release any retained subviews of the main view.
 }
