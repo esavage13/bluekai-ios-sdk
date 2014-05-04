0e0f3771f11036c9746ca6ea5f175a2cac3b7cac
Shun Chu <shun@bluekai.com>
2014-05-04 15:52:27 -0700

    minor syntax update

--- sample/Bluekai/DevSettingsViewController.m
+++ sample/Bluekai/DevSettingsViewController.m
@@ -9,9 +9,9 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+
     if (self) {
         self.tabBarItem.title = @"Settings";
-
     }
     return self;
 }
